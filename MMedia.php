6afeeb94889fa452b224e6b848b9ed822a4f6823
Michał <mwargan@gmail.com>
2019-12-11 07:19:08 +0100

    Update MMedia.php

--- MMedia.php
+++ MMedia.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package M_Media
- * @version 1.0.1
+ * @version 1.1
  */
 /*
 Plugin Name: M Media
 Plugin URI: https://github.com/M-Media-Group/mmedia-wp-plugin
 Description: Required M Media plugin.
 Author: M Media
-Version: 1.0.1
+Version: 1.1
 Author URI: https://mmediagroup.fr/
 License: GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
