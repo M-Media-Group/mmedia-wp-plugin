55c8077d71bad2e3f0d2e4f558bc72a759fe9238
Michał <mwargan@gmail.com>
2019-12-11 08:16:51 +0100

    Update MMedia.php

--- MMedia.php
+++ MMedia.php
@@ -121,7 +121,7 @@ function my_custom_dashboard_widgets()
 
 function custom_dashboard_help()
 {
-    echo '<div style="text-align: center;"><img style="margin:0 auto;" src="' . plugins_url('images/m.svg', __FILE__) . '" height="45"><p>M Media Support is always here and ready to help you with your WordPress website. Not sure about something? Just ask!</p><a class="button button-mmedia" href="https://mmediagroup.fr/contact?utm_source=wordpress&utm_medium=plugin&utm_campaign=' . get_site_url() . '&utm_content=dashboard">Contact us</a> <a class="button" href="/wp-admin/admin.php?page=mmedia_main_menu">More info</a></div>';
+    echo '<div style="text-align: center;"><img style="margin:0 auto;" src="' . plugins_url('images/m.svg', __FILE__) . '" height="45"><p>M Media is always here and ready to help you with your WordPress website. Not sure about something? Just ask!</p><a class="button button-mmedia" href="https://mmediagroup.fr/contact?utm_source=wordpress&utm_medium=plugin&utm_campaign=' . get_site_url() . '&utm_content=dashboard">Contact us</a> <a class="button" href="/wp-admin/admin.php?page=mmedia_main_menu">More info</a></div>';
 }
 
 function m_mime_types($mime_types)
@@ -178,80 +178,6 @@ function wpdocs_enqueue_custom_admin_style()
 }
 add_action('admin_enqueue_scripts', 'wpdocs_enqueue_custom_admin_style');
 
-add_filter('plugins_api', 'mmedia_plugin_info', 20, 3);
-/*
- * $res contains information for plugins with custom update server
- * $action 'plugin_information'
- * $args stdClass Object ( [slug] => woocommerce [is_ssl] => [fields] => Array ( [banners] => 1 [reviews] => 1 [downloaded] => [active_installs] => 1 ) [per_page] => 24 [locale] => en_US )
- */
-function mmedia_plugin_info($res, $action, $args)
-{
-
-    // do nothing if this is not about getting plugin information
-    if ($action !== 'plugin_information') {
-        return false;
-    }
-
-    // do nothing if it is not our plugin
-    if ('YOUR_PLUGIN_SLUG' !== $args->slug) {
-        return $res;
-    }
-
-    // trying to get from cache first, to disable cache comment 18,28,29,30,32
-    if (false == $remote = get_transient('mmedia_upgrade_YOUR_PLUGIN_SLUG')) {
-
-        // info.json is the file with the actual plugin information on your server
-        $remote = wp_remote_get('https://YOUR_WEBSITE/SOME_PATH/info.json', array(
-            'timeout' => 10,
-            'headers' => array(
-                'Accept' => 'application/json',
-            ))
-        );
-
-        if (!is_wp_error($remote) && isset($remote['response']['code']) && $remote['response']['code'] == 200 && !empty($remote['body'])) {
-            set_transient('mmedia_upgrade_YOUR_PLUGIN_SLUG', $remote, 43200); // 12 hours cache
-        }
-
-    }
-
-    if ($remote) {
-
-        $remote = json_decode($remote['body']);
-        $res = new stdClass();
-        $res->name = $remote->name;
-        $res->slug = 'YOUR_PLUGIN_SLUG';
-        $res->version = $remote->version;
-        $res->tested = $remote->tested;
-        $res->requires = $remote->requires;
-        $res->author = '<a href="https://rudrastyh.com">mmedia Rudrastyh</a>'; // I decided to write it directly in the plugin
-        $res->author_profile = 'https://profiles.wordpress.org/rudrastyh'; // WordPress.org profile
-        $res->download_link = $remote->download_url;
-        $res->trunk = $remote->download_url;
-        $res->last_updated = $remote->last_updated;
-        $res->sections = array(
-            'description' => $remote->sections->description, // description tab
-            'installation' => $remote->sections->installation, // installation tab
-            'changelog' => $remote->sections->changelog, // changelog tab
-            // you can add your custom sections (tabs) here
-        );
-
-        // in case you want the screenshots tab, use the following HTML format for its content:
-        // <ol><li><a href="IMG_URL" target="_blank" rel="noopener noreferrer"><img src="IMG_URL" alt="CAPTION" /></a><p>CAPTION</p></li></ol>
-        if (!empty($remote->sections->screenshots)) {
-            $res->sections['screenshots'] = $remote->sections->screenshots;
-        }
-
-        $res->banners = array(
-            'low' => 'https://YOUR_WEBSITE/banner-772x250.jpg',
-            'high' => 'https://YOUR_WEBSITE/banner-1544x500.jpg',
-        );
-        return $res;
-
-    }
-
-    return false;
-
-}
 
 remove_action('wp_head', 'rsd_link');
 remove_action('wp_head', 'wlwmanifest_link');
